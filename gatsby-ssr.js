959681cdd104908c7e808daa7802e4f13f540782
pranavkhapra <pranavkhapra23@gmail.com>
2021-05-20 14:07:20 +0530

    huuuuuuhhh

---
+++ gatsby-ssr.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import './src/styles/global.css';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+// import favicon from '/rainbow.png';
+import Layout from './src/Components/Layout';
+import 'typeface-fira-mono';
+import { ThemeProvider } from './src/Components/ThemeContext';
+
+export const wrapPageElement = ({ element, props }) => (
+  <ThemeProvider>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Pranav Khapra</title>
+      <link rel="icon" type="image/png" href="/rainbow.png" sizes="32x32" />
+    </Helmet>
+    <Layout {...props}>{element}</Layout>
+  </ThemeProvider>
+);
