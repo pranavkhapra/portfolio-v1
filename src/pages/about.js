0be7020fc8fe80ea95daf522240dd3d679751d25
pranavkhapra <pranavkhapra23@gmail.com>
2021-08-21 23:30:07 +0530

    changed about me pic

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,7 +19,7 @@ export default function about() {
           <div className="items-start space-y-2 lg:grid lg:grid-cols-3 lg:space-y-0 lg:pt-4">
             <div className="flex flex-col items-center pt-6 xl:pt-5">
               <img
-                src="/pranav.jpeg"
+                src="/pranav.jpg"
                 alt="avatar"
                 className="w-48 h-48 rounded-xl"
               />
